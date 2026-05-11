a4742399d113b37407898e5193a38559f1531103
Yamamichi <yamamichi.kt9@jsdata.co.jp>
2026-05-11 11:49:12 +0900

    //ddddddddの削除

--- basic-android-kotlin-compose-training-cupcake-navigation/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ basic-android-kotlin-compose-training-cupcake-navigation/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -208,6 +208,5 @@ private fun shareOrder(context: Context, subject: String, summary: String) {
 //qqqqqqqqqqqq
 =======
 //qqqqqqqqqqqqqqqqqqq
-//ddddddddddddddddddddddd
 
 >>>>>>> 01663604d1189598ccdcd7b58de31b734f55a9ad
